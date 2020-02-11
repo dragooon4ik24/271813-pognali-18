50561f9d1a28547f2ef641ac3cf85b7a782a6982
Anton Shafner <ashafner@tsconsulting.com>
2020-02-11 16:28:08 +0300

    Updates

---
+++ precache-manifest.56e6ea689aee7a3b4d0ed3a90b9b1b90.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bdf68d86a5ef3098635edce19a6923a",
+    "url": "/pognali/index.html"
+  },
+  {
+    "revision": "5d875cae5a1ac6e22f28",
+    "url": "/pognali/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "4a5c92fe817bddc034b1",
+    "url": "/pognali/static/css/main.5b41320a.chunk.css"
+  },
+  {
+    "revision": "5d875cae5a1ac6e22f28",
+    "url": "/pognali/static/js/2.da5f3701.chunk.js"
+  },
+  {
+    "revision": "eecdfe5540a0cba768e256ce79f3fa7e",
+    "url": "/pognali/static/js/2.da5f3701.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a5c92fe817bddc034b1",
+    "url": "/pognali/static/js/main.c7ae7b84.chunk.js"
+  },
+  {
+    "revision": "a5b81d1e9df0f173a1f4",
+    "url": "/pognali/static/js/runtime-main.8bf2d8a6.js"
+  },
+  {
+    "revision": "446354c785d5b5dae8017a91b89053e2",
+    "url": "/pognali/static/media/about-desktop@1x.446354c7.png"
+  },
+  {
+    "revision": "bf5f2187006a40b0e4bb3942fa8c4a2a",
+    "url": "/pognali/static/media/about-desktop@2x.bf5f2187.png"
+  },
+  {
+    "revision": "4c78dd111b382a565791a14e4283b6ca",
+    "url": "/pognali/static/media/about-mobile@1x.4c78dd11.png"
+  },
+  {
+    "revision": "52769590d405f1d2923d3e7a9ab42c7a",
+    "url": "/pognali/static/media/about-mobile@2x.52769590.png"
+  },
+  {
+    "revision": "9fc327090032a8e265ead63d1bea9f0d",
+    "url": "/pognali/static/media/about-tablet@1x.9fc32709.png"
+  },
+  {
+    "revision": "ec4bb08b6dce9eaf3136d2b61be19a90",
+    "url": "/pognali/static/media/about-tablet@2x.ec4bb08b.png"
+  },
+  {
+    "revision": "e13e76c38fe282d3ab7ede4c04e22309",
+    "url": "/pognali/static/media/bike.e13e76c3.svg"
+  },
+  {
+    "revision": "591d2ba7389af456169b6e01973736f6",
+    "url": "/pognali/static/media/bloggersans-bold.591d2ba7.woff2"
+  },
+  {
+    "revision": "8b1ea9b53d580c49d8c2817d8f6d157a",
+    "url": "/pognali/static/media/bloggersans-bold.8b1ea9b5.woff"
+  },
+  {
+    "revision": "17084300995d2897ed617ce26ef0ddfd",
+    "url": "/pognali/static/media/bloggersans-light.17084300.woff2"
+  },
+  {
+    "revision": "74cf8ab095000483df550db002dbf90d",
+    "url": "/pognali/static/media/bloggersans-light.74cf8ab0.woff"
+  },
+  {
+    "revision": "012fbecfe550a05f05bac75eb685ae60",
+    "url": "/pognali/static/media/bloggersans-medium.012fbecf.woff"
+  },
+  {
+    "revision": "2a9095107c4fa3db2be699ee42d9d474",
+    "url": "/pognali/static/media/bloggersans-medium.2a909510.woff2"
+  },
+  {
+    "revision": "118dd67135f084434e0935b290322d2b",
+    "url": "/pognali/static/media/bus.118dd671.svg"
+  },
+  {
+    "revision": "0fb434bbbb800368fb6682db8e71dbda",
+    "url": "/pognali/static/media/business.0fb434bb.svg"
+  },
+  {
+    "revision": "8c1dbf4f65f0c65d204d6df750af616c",
+    "url": "/pognali/static/media/contacts-location.8c1dbf4f.svg"
+  },
+  {
+    "revision": "2de7a01109e006b601ee11a17ef36164",
+    "url": "/pognali/static/media/contacts-mail.2de7a011.svg"
+  },
+  {
+    "revision": "c1c83563cd27b2d60d785e009af37db9",
+    "url": "/pognali/static/media/contacts-phone.c1c83563.svg"
+  },
+  {
+    "revision": "88ae193b7e57ee9dc200798df19302cf",
+    "url": "/pognali/static/media/czech-desktop@2x.88ae193b.jpg"
+  },
+  {
+    "revision": "e57a6a6f5b48312a92a1ba34f73291e8",
+    "url": "/pognali/static/media/czech-mobile@2x.e57a6a6f.jpg"
+  },
+  {
+    "revision": "b6edf578b96443eb1150395244d3a9ba",
+    "url": "/pognali/static/media/demin-desktop@2x.b6edf578.jpg"
+  },
+  {
+    "revision": "c80c807733ba5b0c00302b1f1f72934c",
+    "url": "/pognali/static/media/demin-tablet@2x.c80c8077.jpg"
+  },
+  {
+    "revision": "fa050d96de1e2987ab29a1e7dfe06ab9",
+    "url": "/pognali/static/media/facebook.fa050d96.svg"
+  },
+  {
+    "revision": "32c001bd9741b09c5dbe367fc5d4fd38",
+    "url": "/pognali/static/media/firsova-desktop@1x.32c001bd.jpg"
+  },
+  {
+    "revision": "2fe09ff9df49e5d41a461fa4d762cc35",
+    "url": "/pognali/static/media/firsova-desktop@2x.2fe09ff9.jpg"
+  },
+  {
+    "revision": "3510395e81bfaeea2c317fea9770a88e",
+    "url": "/pognali/static/media/firsova-tablet@2x.3510395e.jpg"
+  },
+  {
+    "revision": "d5866e3977aa400327288428a478f6c1",
+    "url": "/pognali/static/media/flag-belgium.d5866e39.svg"
+  },
+  {
+    "revision": "427b70637815f050b5e22f22dc48dc16",
+    "url": "/pognali/static/media/flag-czech.427b7063.svg"
+  },
+  {
+    "revision": "9bc725047a619f4175f4918665f5d871",
+    "url": "/pognali/static/media/flag-france.9bc72504.svg"
+  },
+  {
+    "revision": "1a98215ecb5de3cf72c297554e28e3fd",
+    "url": "/pognali/static/media/flag-seychelles.1a98215e.svg"
+  },
+  {
+    "revision": "a70f230d63cee294291dc72df77f1b38",
+    "url": "/pognali/static/media/flag-sri-lanka.a70f230d.svg"
+  },
+  {
+    "revision": "3c4a3b6c07bcc5e2cccdb7115e3bedfd",
+    "url": "/pognali/static/media/flag-thailand.3c4a3b6c.svg"
+  },
+  {
+    "revision": "2f06d03d0087c1a211c083801d3db2a6",
+    "url": "/pognali/static/media/flags-back@2x.2f06d03d.png"
+  },
+  {
+    "revision": "fe931dac2a149bdf9d8794df9d79117e",
+    "url": "/pognali/static/media/france-desktop@2x.fe931dac.jpg"
+  },
+  {
+    "revision": "a62f082039abc242783d0b904208581b",
+    "url": "/pognali/static/media/france-mobile@2x.a62f0820.jpg"
+  },
+  {
+    "revision": "08353fa6c858fd1a6787e4fafc46fa8e",
+    "url": "/pognali/static/media/globe-back-desktop@2x.08353fa6.jpg"
+  },
+  {
+    "revision": "733a4d5f9355cf885eed1ef6142aea89",
+    "url": "/pognali/static/media/globe-back-tablet@2x.733a4d5f.jpg"
+  },
+  {
+    "revision": "a8122989aa5d49f8ef83ee61103f34a0",
+    "url": "/pognali/static/media/htmlacademy.a8122989.svg"
+  },
+  {
+    "revision": "c1e4e3bb18d5b4cf05b31b1870613410",
+    "url": "/pognali/static/media/instagram.c1e4e3bb.svg"
+  },
+  {
+    "revision": "b1f6028babf124740de4cff3c0751667",
+    "url": "/pognali/static/media/map-desktop@1x.b1f6028b.jpg"
+  },
+  {
+    "revision": "1d0891f66094dfdc7c494a679b9646ed",
+    "url": "/pognali/static/media/map-desktop@2x.1d0891f6.jpg"
+  },
+  {
+    "revision": "2df54f3824dca36f49712154937b3dca",
+    "url": "/pognali/static/media/map-mobile@1x.2df54f38.jpg"
+  },
+  {
+    "revision": "ca8e55ebbdaf7fef16c5fc775a48d50c",
+    "url": "/pognali/static/media/map-mobile@2x.ca8e55eb.jpg"
+  },
+  {
+    "revision": "7ef64615a4aa7c86b3d49e7006be0098",
+    "url": "/pognali/static/media/map-tablet@1x.7ef64615.jpg"
+  },
+  {
+    "revision": "4bda8a8c5db84f1101fbbc296a1082dc",
+    "url": "/pognali/static/media/map-tablet@2x.4bda8a8c.jpg"
+  },
+  {
+    "revision": "a355f2525795a00656ee816165dd3d7d",
+    "url": "/pognali/static/media/plane.a355f252.svg"
+  },
+  {
+    "revision": "fa87f52ba8faa3576009a536dbaaa530",
+    "url": "/pognali/static/media/rogovaya-back-desktop@1x.fa87f52b.png"
+  },
+  {
+    "revision": "d19431bf65c5ba708bcdf326a053088a",
+    "url": "/pognali/static/media/rogovaya-back-desktop@2x.d19431bf.png"
+  },
+  {
+    "revision": "8c6965465e741af10f39db8226bc042c",
+    "url": "/pognali/static/media/rogovaya-back-mobile@2x.8c696546.png"
+  },
+  {
+    "revision": "72a4108b4c456626d240039a7be8500c",
+    "url": "/pognali/static/media/run.72a4108b.svg"
+  },
+  {
+    "revision": "71706e822e6486206255fe935a1948e2",
+    "url": "/pognali/static/media/smolov-back-desktop@1x.71706e82.png"
+  },
+  {
+    "revision": "1544f2c54a221b48f9896797983486ef",
+    "url": "/pognali/static/media/smolov-back-desktop@2x.1544f2c5.png"
+  },
+  {
+    "revision": "fa9cda81b0dbd3913fc04cf3a40e4710",
+    "url": "/pognali/static/media/thailand-desktop@2x.fa9cda81.jpg"
+  },
+  {
+    "revision": "6e2fb43577df2cfc09d80b997d8257e8",
+    "url": "/pognali/static/media/thailand-mobile@2x.6e2fb435.jpg"
+  },
+  {
+    "revision": "cb7f8505995f537aaf7a9f29ebc2e357",
+    "url": "/pognali/static/media/topmenu-mail.cb7f8505.svg"
+  },
+  {
+    "revision": "a28859b3f4dc7a02c7f12d60e354d704",
+    "url": "/pognali/static/media/topmenu-phone.a28859b3.svg"
+  },
+  {
+    "revision": "982fc4a1e08fadce6b9883473cff5770",
+    "url": "/pognali/static/media/traveller-illustration-desktop@2x.982fc4a1.png"
+  },
+  {
+    "revision": "832b34fb477541a7a0eb14f1360f6791",
+    "url": "/pognali/static/media/traveller-illustration-tablet@2x.832b34fb.png"
+  },
+  {
+    "revision": "527261133a6cccfd5a4ebd70f4d68ea2",
+    "url": "/pognali/static/media/user.52726113.svg"
+  },
+  {
+    "revision": "ca58be2975ade4c6bb1ce4e4c235417a",
+    "url": "/pognali/static/media/vk.ca58be29.svg"
+  }
+]);
